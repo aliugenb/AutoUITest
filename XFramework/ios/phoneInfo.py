57693ee6fda3c688d6bc939ede152f1a70b81868
huili.jiang <karen.jiang@ximalaya.com>
2018-06-04 11:19:25 +0800

    修改phoneinfo

--- XFramework/ios/phoneInfo.py
+++ XFramework/ios/phoneInfo.py
@@ -2,16 +2,29 @@
 from appium import webdriver
 from baseOn import BaseOn as bo
 
+configData_default = {}
+configData_default["platformName"] = "ios"
+configData_default["newCommandTimeout"] = 3000
+configData_default["testAppPath"] = '/Users/nali/Downloads/ting-6.3.72-666.ipa'
 
-def getDriver(configData):
+
+
+def getDriver(configData=configData_default):
+    app = os.path.join(os.path.dirname(__file__), configData[testAppPath])
+    app = os.path.abspath(app)   
+
     desired_caps = {}
-    desired_caps['automationName'] = 'UiAutomator2'  # 使用UI2
-    desired_caps['platformName'] = configData['platformName']  # 设备系统
-    desired_caps['platformVersion'] = bo().getPlatformVersion()  # 设备系统版本
-    desired_caps['deviceName'] = bo().getDeviceName()  # 设备名称
-    desired_caps['appPackage'] = configData['appPackage']
-    desired_caps['appActivity'] = configData['appActivity']
-    desired_caps['newCommandTimeout'] = configData['newCommandTimeout']
+    desired_caps['platformName'] = configData['platformName'] 
+    desired_caps['platformVersion'] = bo.getPlatformVersionIos()
+    desired_caps['deviceName'] = bo.getPhoneNameIos()
+    desired_caps['app']=app
+    #desired_caps['udid']='2e58ffd37a53a8a3920f51b4ab73fe5e6a363d22'
+    desired_caps['udid']=bo.getPhoneUdid()
+    desired_caps['automationName']='XCUITest'
+    desired_caps['newCommandTimeout']=configData['newCommandTimeout']
+    desired_caps['allowTouchIdEnroll']=True
+    desired_caps['noReset'] = True
     desired_caps["unicodeKeyboard"] = True  # 支持中文
     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-    return driver
+    
+    return desired_caps
