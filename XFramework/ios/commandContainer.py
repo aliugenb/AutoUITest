e46b70f4aba4d12a197cf7aca05d6387c37ab005
nali <nali@nalideMacBook-Pro.local>
2018-06-12 10:20:12 +0800

    fix

--- XFramework/ios/commandContainer.py
+++ XFramework/ios/commandContainer.py
@@ -9,8 +9,8 @@ class CommandContainer(object):
     #获取手机的设备版本：
     PLATFORM_VERSION = 'ideviceinfo -k ProductVersion'
     #获取手机的设备名：
-    PHONE_NAME = 'ideviceinfo -k ProductType'   
-        
+    PHONE_NAME = 'ideviceinfo -k ProductType'
+
     XIMALAYA_PKG = 'com.gemd.iting'
-   
+
     SPECIAL_CHARACTER_LIST = r'[\\/:*?" <>|]'
