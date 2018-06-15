00141438582818327572927ee858e57624fa5b5a
huili.jiang <karen.jiang@ximalaya.com>
2018-06-15 17:41:54 +0800

    增加路径自动获取

--- XFramework/ios/commandContainer.py
+++ XFramework/ios/commandContainer.py
@@ -10,7 +10,13 @@ class CommandContainer(object):
     PLATFORM_VERSION = 'ideviceinfo -k ProductVersion'
     #获取手机的设备名：
     PHONE_NAME = 'ideviceinfo -k ProductType'
+    #获取手机截图：
+    SCREENCAP = 'idevicescreenshot'
+    
+    PROJECTPATH = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+    PIC_SAVEPATH = PROJECTPATH +'/testDriver/screencap/' 
 
     XIMALAYA_PKG = 'com.gemd.iting'
+    
 
     SPECIAL_CHARACTER_LIST = r'[\\/:*?" <>|]'
