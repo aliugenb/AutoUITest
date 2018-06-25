52a6bdd61f8e0fbe4cb05b67bac4225c04f91638
nali <nali@nalideMacBook-Pro.local>
2018-06-25 12:52:10 +0800

    fix

--- XFramework/android/TestRunner.py
+++ XFramework/android/TestRunner.py
@@ -291,7 +291,7 @@ def actionHandle(control, data, realAction, uiObj, imgDict):
                                                     imgDict['srcImgPath'],
                                                     imgDict['realSrcImgName']),
                                                targetImgName,
-                                               confidence=0.54)
+                                               confidence=0.55)
                 if reInfo is not None:
                     break
                 time.sleep(1)
